651d09497eca42766e915f7cee6d86fa5b42b68d
codeout <goodies@codeout.net>
2014-06-22 23:08:30 +0900

    Bugfix: Removed unwanted changes which had been mistakenly committed

--- lib/irrc/query.rb
+++ lib/irrc/query.rb
@@ -1,6 +1,5 @@
 require 'irrc/irr'
 require 'irrc/query_status'
-require 'irrc/subquery'
 
 module Irrc
 
@@ -8,7 +7,6 @@ module Irrc
   class Query
     include Irrc::Irr
     include Irrc::QueryStatus
-    include Irrc::Subquery
 
     attr_reader :sources, :protocols
 
