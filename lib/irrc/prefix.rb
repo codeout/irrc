2e474e058ffec584ff77f10f6119b04e31d23d63
Shintaro Kojima <goodies@codeout.net>
2017-01-16 01:36:18 +0900

    Accept non-Array object

--- lib/irrc/prefix.rb
+++ lib/irrc/prefix.rb
@@ -5,7 +5,7 @@ module Irrc
     private
 
     def classify_by_protocol(prefixes)
-      prefixes.each_with_object(Struct.new(:ipv4, :ipv6).new([], [])) {|prefix, result|
+      Array(prefixes).each_with_object(Struct.new(:ipv4, :ipv6).new([], [])) {|prefix, result|
         addr = IPAddr.new(prefix)
         if addr.ipv4?
           result.ipv4 << prefix
